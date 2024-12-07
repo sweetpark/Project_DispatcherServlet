5f4a8bfc3223b6c59cba1c2de890893c164c2a28
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-12-07 16:41:38 +0900

    ROOT WAR 추가

--- src/main/java/example/Servlet/ServletApplication.java
+++ src/main/java/example/Servlet/ServletApplication.java
@@ -3,16 +3,21 @@ package example.Servlet;
 import example.Servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan
 @SpringBootApplication
-public class ServletApplication {
+public class ServletApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServletApplication.class);
+	}
 }
